a9f2ad984c64dc00e4b5902ea7f1a3db2b415961
Matt Robitaille <matthew@robitaille.io>
2013-07-05 16:30:39 -0400

    add basic header and footer includes

---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title>Simple PDO</title>
+
+    <link rel="stylesheet" type="text/css" href="layout/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <h1><a href="/">Simple PDO</a></h1>
